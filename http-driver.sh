7616102cf3a574d95c2de0de705fed6af1ab6b22
Blastradius <>
2022-03-24 23:29:35 +0100

    Randomize service name

--- http-driver.sh
+++ http-driver.sh
@@ -1,5 +1,59 @@
-cp http-driver.service /etc/systemd/system/http-driver.service
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o bin
+#!/bin/bash
+echo "================================================================================"
+echo "This installation script will register a system service. When finished, the name"
+echo "of this service is readable by any user. To make sure that it cannot be used for"
+echo "detection purposes, you have to enter a random service name. Ensure that it does"
+echo "not exist already, and only use characters in [0-9A-Z-]."
+echo "================================================================================"
+read -p "Name: " serviceName
+
+# ====================
+#
+# ====================
+rootPath="/root/${serviceName}"
+execPath="/root/${serviceName}/${serviceName}"
+servPath="/etc/systemd/system/${serviceName}.service"
+
+# ====================
+# 
+# ====================
+rm -rf "bin"
+rm -rf "$rootPath"
+
+# ====================
+# 
+# ====================
+dotnet publish --output "${rootPath}" --runtime linux-x64 --self-contained \
+  "-p:Configuration=Release;AssemblyName=${serviceName}" \
+  "-p:DebugType=None" \
+  "-p:GenerateRuntimeConfigurationFiles=true" \
+  "-p:PublishSingleFile=true"
+
+# ====================
+# 
+# ====================
+cat > $servPath << EOF
+[Unit]
+Description=${serviceName}
+
+[Service]
+Type=notify
+WorkingDirectory=${rootPath}
+ExecStart=${execPath}
+Environment=ASPNETCORE_URLS=http://*:8080/
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# ====================
+# 
+# ====================
+chmod 770 "$servPath"
+
+# ====================
+# 
+# ====================
 systemctl daemon-reload
-systemctl start http-driver
-systemctl enable http-driver
+systemctl start "${serviceName}"
+systemctl enable "${serviceName}"
