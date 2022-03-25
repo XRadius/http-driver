d9b91899381fba33af0b894ae438dbb82b40872e
Blastradius <>
2022-03-25 09:35:58 +0100

    Rewrite memory write to enable batch writing

--- src/Controllers/MemoryController.cs
+++ src/Controllers/MemoryController.cs
@@ -19,13 +19,13 @@ public class MemoryController : Controller {
   public async Task GetAsync(int pid, string request) {
     Response.Headers.Add("Content-Type", "application/octet-stream");
     var maps = request.Split(',');
-    for (var i = 0; i < maps.Length; i++) {
+    for (ushort i = 0; i < maps.Length; i++) {
       var match = me.Match(maps[i]);
       if (match.Success) {
         var address = Parse(match.Groups[1].Value);
         var buffer = new byte[Parse(match.Groups[2].Value)];
         if (memoryService.Read(pid, address, buffer)) {
-          await Response.Body.WriteAsync(BitConverter.GetBytes((ushort) i), 0, 2);
+          await Response.Body.WriteAsync(BitConverter.GetBytes(i));
           await Response.Body.WriteAsync(buffer, 0, buffer.Length);
         }
       }
@@ -36,11 +36,21 @@ public class MemoryController : Controller {
   [OpenApiBodyParameter("application/octet-stream")]
   [ProducesResponseType(typeof(void), 204)]
   [ProducesResponseType(typeof(void), 404)]
-  [Route("proc/{pid}/mem/{address}")]
-  public async Task<IActionResult> PutAsync(int pid, string address) {
-    var buffer = new MemoryStream();
-    await Request.Body.CopyToAsync(buffer);
-    return memoryService.Write(pid, Parse(address), buffer.ToArray()) ? NoContent() : NotFound();
+  [Route("proc/{pid}/mem/{request}")]
+  public async Task PutAsync(int pid, string request) {
+    Response.Headers.Add("Content-Type", "application/octet-stream");
+    var maps = request.Split(',');
+    for (ushort i = 0; i < maps.Length; i++) {
+      var match = me.Match(maps[i]);
+      if (match.Success) {
+        var address = Parse(match.Groups[1].Value);
+        var buffer = new byte[Parse(match.Groups[2].Value)];
+        await Request.Body.ReadAsync(buffer, 0, buffer.Length);
+        if (memoryService.Write(pid, address, buffer)) {
+          await Response.Body.WriteAsync(BitConverter.GetBytes(i));
+        }
+      }
+    }
   }
 
   private ulong Parse(string? value) {
